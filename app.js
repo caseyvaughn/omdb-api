f8dceaf41623f789fc703edcafbde7e8a3671723
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2021-12-08 19:11:32 -0500

    creating event listener

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const DOMAIN = 'http://www.omdbapi.com/';
-const API_KEY = /* Your API key goes here */
+const API_KEY = "6e111b46"
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
 
 
+// - Attach an event listener that simply logs the text value of the input
+let textInput = document.querySelector("#blank");
+let searchButton = document.querySelector("#search");
+
+console.log(textInput);
+
+const logText = (event) => {
+  event.preventDefault();
+
+  let value = textInput.value;
+  console.log(value);
+}
+
+textInput.addEventListener("submit", logText);
+
+
+//   - Next, add the `axios` api call.For searching, the url should look something like this: `http://www.omdbapi.com/?apikey=[yourkey]&s=[movietitle]`
+//     - Remember to use `async` and `await` for the axios calls
+//       - Try to `console.log` the search results.Or use the `network` tab in the dev tools + the preview tab for a request ot view its structure.
+// ![dev tools](./devtools.png)
